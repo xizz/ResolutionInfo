753d38ee1d3d097b93fffbfa2c18fa23920e04c5
Xi Wei <xwei@handy.com>
2016-05-08 18:26:33 -0400

    added dp info

--- app/src/main/java/com/xizz/resolutioninfo/MainActivity.java
+++ app/src/main/java/com/xizz/resolutioninfo/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class MainActivity extends Activity {
 
 	@Override
@@ -13,21 +16,27 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		DisplayMetrics metrics = getResources().getDisplayMetrics();
-		float density = metrics.density * 160;
-		float width = metrics.widthPixels / density;
-		float height = metrics.heightPixels / density;
-		double diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+		float screenWidthInches = metrics.widthPixels / (float) metrics.densityDpi;
+		float screenHeightInches = metrics.heightPixels / (float) metrics.densityDpi;
+		float screenWidthDp = metrics.widthPixels / metrics.density;
+		float screenHeightDp = metrics.heightPixels / metrics.density;
+		double diagonal = Math.sqrt(Math.pow(screenWidthInches, 2) + Math.pow(screenHeightInches, 2));
 
+		TextView diagonalText = (TextView) findViewById(R.id.text_diagonal);
+		TextView sizeText = (TextView) findViewById(R.id.text_size);
+		TextView resolutionDpText = (TextView) findViewById(R.id.text_resolution_dp);
+		TextView resolutionPxText = (TextView) findViewById(R.id.text_resolution_px);
+		TextView densityText = (TextView) findViewById(R.id.text_density);
 
-		TextView txtDiagonal = (TextView) findViewById(R.id.textViewDiagonal);
-		TextView txtSize = (TextView) findViewById(R.id.textViewSize);
-		TextView txtResolution = (TextView) findViewById(R.id.textViewResolution);
-		TextView txtDensity = (TextView) findViewById(R.id.textViewDensity);
+		NumberFormat format = new DecimalFormat("#.#");
 
-		txtDiagonal.setText(String.format("%.1f", diagonal));
-		txtSize.setText(String.format("%.1f", width) + " x "
-				+ String.format("%.1f", height));
-		txtResolution.setText(metrics.widthPixels + " x " + metrics.heightPixels);
-		txtDensity.setText(String.format("%.0f", density));
+		diagonalText.setText(format.format(diagonal));
+		sizeText.setText(getString(R.string.x_formatted,
+				format.format(screenWidthInches), format.format(screenHeightInches)));
+		resolutionDpText.setText(getString(R.string.x_formatted,
+				format.format(screenWidthDp), format.format(screenHeightDp)));
+		resolutionPxText.setText(getString(R.string.x_formatted,
+				metrics.widthPixels, metrics.heightPixels));
+		densityText.setText(String.valueOf(metrics.densityDpi));
 	}
 }
